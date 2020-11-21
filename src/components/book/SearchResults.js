840de1e903ef9799b6642069a9ed4332bdd2dcaa
saga <wlatmdqja@naver.com>
2020-11-21 21:20:55 +0900

    카카오 검색 결과 리스트 컴포넌트 리팩토링

--- src/components/book/SearchResults.js
+++ src/components/book/SearchResults.js
@@ -3,8 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { Card, Col, Row, Empty } from "antd";
 import Paginationbar from "./Paginationbar";
 import { changeSaveBookFlag, chagneFlagListIndex } from "../../modules/book";
+import KakaoBookSearchRow from './KakaoBookSearchRow';
 const SearchResults = () => {
   const dispatch = useDispatch();
+  const [leni ,setLeni] = useState(0);
   const { Meta, Grid } = Card;
   const [len, setLen] = useState(0);
   const [cardLoading, setCardLoading] = useState(false);
@@ -18,7 +20,7 @@ const SearchResults = () => {
   } = useSelector((state) => state.book);
 
   const handleFlag = (e) => {
-    console.log(e);
+    console.log('index ',e,' 가 클릭되었습니다.')
     dispatch(chagneFlagListIndex(e));
   };
 
@@ -42,87 +44,12 @@ const SearchResults = () => {
           <Empty description="검색결과가 없습니다." />
         </Row>
       )}
+      
+      <KakaoBookSearchRow rowIndex= {0} colSize ={4}/>
+      <br></br>
+      <KakaoBookSearchRow rowIndex= {4} colSize ={8}/>
 
-      <Row justify="center" align="center" gutter={16}>
-        {kakaoBookResult != null &&
-          kakaoBookResult.documents.slice(0, size / 2).map((book, idx) => {
-            return (
-              <Col span={4} key={idx}>
-                <Card
-                  onClick={() => handleFlag(idx)}
-                  size="small"
-                  key={idx}
-                  hoverable
-                  style={{
-                    width: "50%",
-                    margin: "auto",
-                    opacity: saveBookFlag === true ? "0.8" : "1",
-                    border:
-                      saveBookFlag && saveBookList[idx] === true
-                        ? "3px solid rgb(33,139,255)"
-                        : null,
-                  }}
-                  cover={
-                    <img
-                      alt="example"
-                      style={{ width: "95%", margin: "auto" }}
-                      src={book.thumbnail}
-                    />
-                  }
-                >
-                  <Meta
-                    key={idx}
-                    title={book.title}
-                    description={book.publisher}
-                  />
-                </Card>
-              </Col>
-            );
-          })}
-      </Row>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Row justify="center" align="center" gutter={16}>
-        {kakaoBookResult != null &&
-          kakaoBookResult.documents.slice(size / 2).map((book, idx) => {
-            return (
-              <Col span={4} key={idx}>
-                <Card
-                  onClick={() => handleFlag(idx + size)}
-                  key={idx}
-                  size="small"
-                  hoverable
-                  style={{
-                    width: "50%",
-                    margin: "auto",
-                    opacity: saveBookFlag === true ? "0.8" : "1",
-                    border:
-                      saveBookFlag && saveBookList[idx + size] === true
-                        ? "3px solid rgb(33,139,255)"
-                        : null,
-                  }}
-                  cover={
-                    <img
-                      alt="example"
-                      style={{ width: "95%", margin: "auto" }}
-                      src={book.thumbnail}
-                    />
-                  }
-                >
-                  <Meta
-                    key={idx}
-                    title={book.title}
-                    description={book.publisher}
-                  />
-                </Card>
-              </Col>
-            );
-          })}
-      </Row>
-      <br></br>
-      <br></br>
-      <br></br>
+<br></br>
       {kakaoBookResult != null && kakaoBookResult.documents.length !== 0 && (
         <Row justify="center">
           <Paginationbar total={kakaoBookResult.meta.pageable_count / size} />
--- src/lib/book.js
+++ src/lib/book.js
@@ -2,13 +2,13 @@ import axios from "axios";
 const url = "http://localhost:8080";
 
 export const searchKakaoBook = (param) => {
-  console.log("api", param);
   return axios.get(`${url}/feign/kakao-book-search`, {
     params: {
       query: param.query,
       page: param.page,
       size: param.size,
       target: param.target,
+      sort : 'accuracy'
     },
   });
 };
